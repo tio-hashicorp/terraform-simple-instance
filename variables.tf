a07ef01dba6297c99443af7190fef4bcc1ef2102
Kevin Cochran <kcochran@hashicorp.com>
2020-01-31 20:13:10 -0500

    update variable details

--- variables.tf
+++ variables.tf
@@ -8,10 +8,14 @@ variable "aws_secret_key" {
 
 variable "aws_region" {
     description = "AWS Region"
+    default = "us-east-1"
 }
 
 variable "key_pair" {
     description = "Key pair used to login to the instance"
 }
 
-variable "instance_type" { }
+variable "instance_type" {
+    description = "Instance type for the simple instance"
+    default = "t3.large"
+}
